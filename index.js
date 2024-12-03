d82642f8527843cacb8fc82f6651fa793ba750c5
Sirus Salari <salaris@oregonstate.edu>
2024-12-02 21:19:01 -0800

    Use preferences routes in index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const preferenceRoutes = require('./routes/preferenceRoutes');
 
 dotenv.config();
 const app = express();
@@ -12,6 +13,8 @@ app.use(bodyParser.json());
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Error connecting to MongoDB', err));
+
+app.use(preferenceRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
