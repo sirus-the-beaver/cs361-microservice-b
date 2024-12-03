8380f15e131ff6fb71d6c716155df1e7f032b1f8
Sirus Salari <salaris@oregonstate.edu>
2024-12-02 21:37:03 -0800

    Use express.json instead of bodyParser

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const preferenceRoutes = require('./routes/preferenceRoutes');
 
 dotenv.config();
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
