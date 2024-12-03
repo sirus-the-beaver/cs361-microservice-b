6dd04d1c21f8b67f0d6159daf6d358348858eaf9
Sirus Salari <salaris@oregonstate.edu>
2024-12-02 21:15:01 -0800

    Use cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
