f72dcaa4b0143f034edd1b21e07a234aed32d1f6
Sirus Salari <salaris@oregonstate.edu>
2024-12-02 18:28:45 -0800

    Define controllers for saving/updating and getting preferences

---
+++ controllers/preferenceController.js
@@ -0,0 +1,36 @@
+const Preference = require('../models/Preference');
+
+exports.savePreferences = async (req, res) => {
+    const { userId, dietaryRestrictions, allergies } = req.body;
+    try {
+        const existingPreference = await Preference.findOne({ userId });
+        if (existingPreference) {
+            exisitingPreference.dietaryRestrictions = dietaryRestrictions || [];
+            exisitingPreference.allergies = allergies || [];
+            await existingPreference.save();
+        } else {
+            const newPreference = new Preference({ userId, dietaryRestrictions, allergies });
+            await newPreference.save();
+        }
+
+        res.status(200).send('Preferences saved successfully');
+    } catch (error) {
+        console.error('Error saving preferences', error);
+        res.status(500).json({ error: 'Error saving preferences' });
+    }
+};
+
+exports.getPreferences = async (req, res) => {
+    const { userId } = req.params;
+    try {
+        const preference = await Preference.findOne({ userId });
+        if (preference) {
+            res.status(200).json(preference);
+        } else {
+            res.status(404).json({ error: 'Preferences not found' });
+        }
+    } catch (error) {
+        console.error('Error getting preferences', error);
+        res.status(500).json({ error: 'Error getting preferences' });
+    }
+};
