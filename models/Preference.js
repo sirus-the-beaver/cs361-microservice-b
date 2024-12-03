213e039a38860ed87677a4dc582fb89d4684f72d
Sirus Salari <salaris@oregonstate.edu>
2024-12-02 20:57:19 -0800

    Change type of userId field to mongoose objectId type

--- models/Preference.js
+++ models/Preference.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const preferenceSchema = new mongoose.Schema({
     userId: {
-        type: String,
+        type: mongoose.Schema.Types.ObjectId,
         required: true,
         unique: true
     },
