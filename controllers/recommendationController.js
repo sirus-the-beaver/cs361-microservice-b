36acd1d3b4dcdde50f6991d210444fc7042c83ae
Sirus Salari <salaris@oregonstate.edu>
2024-12-03 17:24:25 -0800

    If no preferences, send a request to spoonacular without preferences

--- controllers/recommendationController.js
+++ controllers/recommendationController.js
@@ -6,29 +6,41 @@ exports.getRecommendations = async (req, res) => {
     const ingredients = req.body.ingredients;
     const preferences = await Preference.findOne({ userId });
 
-    if (!preferences) {
-        return res.status(404).json({ error: 'Preferences not found' });
-    }
-
     if (!ingredients) {
         return res.status(400).json({ error: 'No ingredients provided' });
     }
 
-    try {
-        const response = await axios.get('https://api.spoonacular.com/recipes/complexSearch', 
-            {
-                params: {
-                    ingredients: ingredients.join(','),
-                    ranking: 2,
-                    apiKey: process.env.SPOONACULAR_API_KEY,
-                    diet: preferences.dietaryRestrictions.join(','),
-                    intolerances: preferences.allergies.join(','),
+    if (!preferences) {
+        try {
+            const response = await axios.get('https://api.spoonacular.com/recipes/complexSearch', 
+                {
+                    params: {
+                        ingredients: ingredients.join(','),
+                        ranking: 2,
+                        apiKey: process.env.SPOONACULAR_API_KEY
+                    }
                 }
-            }
-        );
+            );
+        } catch (error) {
+            return res.status(500).json({ error: 'Error fetching recommendations' });
+        }
+    } else {
+        try {
+            const response = await axios.get('https://api.spoonacular.com/recipes/complexSearch', 
+                {
+                    params: {
+                        ingredients: ingredients.join(','),
+                        ranking: 2,
+                        apiKey: process.env.SPOONACULAR_API_KEY,
+                        diet: preferences.dietaryRestrictions.join(',') || 'none',
+                        intolerances: preferences.allergies.join(',') || 'none',
+                    }
+                }
+            );
 
-        return res.status(200).json(response.data.results);
-    } catch (error) {
-        return res.status(500).json({ error: 'Error fetching recommendations' });
+            return res.status(200).json(response.data.results);
+        } catch (error) {
+            return res.status(500).json({ error: 'Error fetching recommendations with preferences' });
+        }
     }
 };
