21248446b2d17355bbeb62e9a135dc490297dad0
Sirus Salari <salaris@oregonstate.edu>
2024-12-09 11:54:17 -0800

    Allow credentials

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.use(cors(
                 return callback(new Error(msg), false);
             }
             return callback(null, true);
-        }
+        },
+        credentials: true
     }
 ));
 app.use(express.json());
